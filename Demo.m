67d4ec7c54525c231d8aba79f45e4fc8eaca5d3c
Kasra Rafiezadeh Shahi <74023741+Kasra2020@users.noreply.github.com>
2021-01-04 19:42:22 +0100

    Update Demo.m

--- Demo.m
+++ Demo.m
@@ -11,4 +11,4 @@ depth = 3; % The depth for exploring the data for the automatic version of HESSC
 
 %% Manual HESSC
 % n_cluster = 5; % Numeber of clusters for the manual version of HESSC.
-% [CSmap, Ptime] = HESSC_fm(img,5);
+% [CSmap, Ptime] = HESSC_fm(img,n_cluster);
