93644fa69a7c82fb68d3828544516399100638b6
Kasra Rafiezadeh Shahi <74023741+Kasra2020@users.noreply.github.com>
2021-01-04 19:41:47 +0100

    Add files via upload

---
+++ Demo.m
@@ -0,0 +1,14 @@
+clear all;
+close all;
+clc;
+
+%% Add the dependencies
+addpath (genpath('./Functions'))
+
+%% Automatic HESSC
+depth = 3; % The depth for exploring the data for the automatic version of HESSC.
+[CSmap,Ptime, n_cluster] = HESSC_fauto(img,depth);
+
+%% Manual HESSC
+% n_cluster = 5; % Numeber of clusters for the manual version of HESSC.
+% [CSmap, Ptime] = HESSC_fm(img,5);
